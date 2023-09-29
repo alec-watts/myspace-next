9eedd332f27bff16e1516d3f7304e2244370933a
alec-watts <alecwatts1@gmail.com>
2023-09-29 18:28:14 -0400

    finiished error

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -2,6 +2,7 @@ import { prisma } from "@/lib/prisma";
 import UserCard from "@/components/UserCard/UserCard";
 
 export default async function Users() {
+  // throw new Error('This is an error')
   const users = await prisma.user.findMany();
   return (
     <div>
