e00d45a06f6af34a9f69cecbf094b9f4e8ad6d3a
alec-watts <alecwatts1@gmail.com>
2023-09-30 13:42:26 -0400

    f

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,26 @@
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import { ProfileForm } from "./ProfileForm";
+import { SignOutButton } from "@/components/buttons";
+
+export default async function Dashboard() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) redirect("/api/auth/signin");
+
+  const currentUserEmail = session?.user?.email!;
+  const user = await prisma.user.findUnique({
+    where: { email: currentUserEmail },
+  });
+  console.log(user);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <SignOutButton />
+      <ProfileForm user={user} />
+    </>
+  );
+}
