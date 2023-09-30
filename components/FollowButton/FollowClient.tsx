70cc46b60152d3b5e0b17358f1efad3afb6b1020
alec-watts <alecwatts1@gmail.com>
2023-09-30 18:14:02 -0400

    done

--- components/FollowButton/FollowClient.tsx
+++ components/FollowButton/FollowClient.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useTransition } from "react";
+import { experimental_useOptimistic as useOptimistic } from "react";
 import { follow, unfollow } from "./actions";
 
 interface Props {
@@ -9,21 +10,59 @@ interface Props {
 }
 
 export default function FollowClient({ targetUserId, isFollowing }: Props) {
-  const [isPending, startTransition] = useTransition();
+  // Optomistic Implementation
+  const [optomisticFollowing, toggleOptomisticFollow] = useOptimistic(
+    {
+      isFollowing,
+      sending: false,
+    },
+    (state, newIsFollowing) => ({
+      ...state,
+      isFollowing: !!newIsFollowing,
+      sending: true,
+    })
+  );
 
-  if (!isFollowing) {
-    return (
-      <button onClick={() => startTransition(() => follow(targetUserId))}>
-        {"Follow"}
+  return (
+    <>
+      <button
+        onClick={async () => {
+          toggleOptomisticFollow(!optomisticFollowing.isFollowing);
+          if (!isFollowing) {
+            follow(targetUserId);
+          } else {
+            unfollow(targetUserId);
+          }
+        }}
+      >
+        {optomisticFollowing.isFollowing ? "Unfollow" : "Follow"}
       </button>
-    );
-  }
+      <br />
+      {"Actual Status: "}
+      {isFollowing ? "Unfollow" : "Follow"}
+      <br />
+      {optomisticFollowing.sending ? "Sending..." : ""}
+    </>
+  );
 
-  if (isFollowing) {
-    return (
-      <button onClick={() => startTransition(() => unfollow(targetUserId))}>
-        {"Unfollow"}
-      </button>
-    );
-  }
+  // // Transition Implementation
+  // const [isPending, startTransition] = useTransition();
+
+  // if (!isFollowing) {
+  //   return (
+  //     <>
+  //       <button onClick={() => startTransition(() => follow(targetUserId))}>
+  //         {"Follow"}
+  //       </button>
+  //     </>
+  //   );
+  // }
+
+  // if (isFollowing) {
+  //   return (
+  //     <button onClick={() => startTransition(() => unfollow(targetUserId))}>
+  //       {"Unfollow"}
+  //     </button>
+  //   );
+  // }
 }
